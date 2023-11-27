62fa498a1253a2e93a35e3278b1dcf0730c045a9
jeongjiwon <jungkt0x@naver.com>
2023-11-27 23:05:15 +0900

    feat: 프로젝트 생성 및 DB 설정

---
+++ src/main/java/com/kanvan/KanvanApplication.java
@@ -0,0 +1,13 @@
+package com.kanvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanvan/KanvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
