7cd616e10eff320eb9b55dfc4ffdd0a94c59ef23
jeongjiwon <jungkt0x@naver.com>
2023-11-29 14:28:49 +0900

    refactor: security 설정 변경

--- src/main/java/com/kanvan/auth/config/SecurityConfig.java
+++ src/main/java/com/kanvan/auth/config/SecurityConfig.java
@@ -1,28 +1,35 @@
 package com.kanvan.auth.config;
 
+import com.kanvan.auth.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(POST, "/api/signup").permitAll()
-                        .requestMatchers(POST, "/api/login").permitAll()
+                        .requestMatchers(POST, "/api/auth/signup").permitAll()
+                        .requestMatchers(POST, "/api/auth/login").permitAll()
+                        //.requestMatchers(GET, "/api/test").permitAll()
                         .anyRequest().authenticated()
                 )
 
@@ -30,11 +37,8 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
