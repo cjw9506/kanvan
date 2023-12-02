87b559b02e141464266ee17e012dcb8b5dc1af5b
jeongjiwon <jungkt0x@naver.com>
2023-12-03 00:17:35 +0900

    feat: 티켓 엔티티 작성

---
+++ src/main/java/com/kanvan/ticket/domain/Ticket.java
@@ -0,0 +1,48 @@
+package com.kanvan.ticket.domain;
+
+import com.kanvan.column.domain.Columns;
+import com.kanvan.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Ticket {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int ticketOrder;
+
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private Tag tag;
+
+    private String workingTime;
+
+    private String deadline;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User manager;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "column_id")
+    private Columns column;
+
+    @Builder
+    public Ticket(int ticketOrder, String title, Tag tag, String workingTime,
+                  String deadline, User manager, Columns column) {
+        this.ticketOrder = ticketOrder;
+        this.title = title;
+        this.tag = tag;
+        this.workingTime = workingTime;
+        this.deadline = deadline;
+        this.manager = manager;
+        this.column = column;
+    }
+}
