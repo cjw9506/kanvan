a9f7ca36f61c5a5d2bd0e086f019cae2682796c2
jeongjiwon <jungkt0x@naver.com>
2023-12-05 16:41:24 +0900

    style: 사용하지 않은 메서드 삭제

--- src/main/java/com/kanvan/team/repository/MemberRepository.java
+++ src/main/java/com/kanvan/team/repository/MemberRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByMemberAndTeam(User user, Team team);
 
     List<Member> findByMember(User user);
 
