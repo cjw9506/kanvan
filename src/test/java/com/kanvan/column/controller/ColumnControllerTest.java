7d42b30142dc943f61c3a5a142b325eed212229c
jeongjiwon <jungkt0x@naver.com>
2023-12-11 20:28:28 +0900

    test: 컬럼 목록 조회 테스트 추가

--- src/test/java/com/kanvan/column/controller/ColumnControllerTest.java
+++ src/test/java/com/kanvan/column/controller/ColumnControllerTest.java
@@ -3,7 +3,10 @@ package com.kanvan.column.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kanvan.auth.filter.JwtAuthenticationFilter;
 import com.kanvan.column.dto.ColumnCreateRequest;
+import com.kanvan.column.dto.ColumnsResponse;
+import com.kanvan.column.dto.TicketResponse;
 import com.kanvan.column.service.ColumnService;
+import com.kanvan.ticket.domain.Tag;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,5 +67,61 @@ class ColumnControllerTest {
 
     }
 
+    @DisplayName("컬럼 목록 조회 - 티켓 x")
+    @WithMockUser
+    @Test
+    void getColumns() throws Exception {
 
+        ColumnsResponse column1 = ColumnsResponse.builder()
+                .columnId(1L)
+                .name("Todo")
+                .columnOrder(1)
+                .build();
+
+        List<ColumnsResponse> response = List.of(column1);
+
+        when(columnService.getColumns(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/api/teams/1/columns").with(csrf())
+                        .contentType(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(columnService).getColumns(any(Long.class));
+
+    }
+
+    @DisplayName("컬럼 목록 조회 - 티켓 포함")
+    @WithMockUser
+    @Test
+    void getColumns2() throws Exception {
+
+        TicketResponse ticket = TicketResponse.builder()
+                .ticketOrder(1)
+                .title("column test")
+                .tag(Tag.BACKEND)
+                .workingTime("3 hours")
+                .deadline("oct 12")
+                .memberAccount("account1")
+                .build();
+
+        ColumnsResponse column1 = ColumnsResponse.builder()
+                .columnId(1L)
+                .name("Todo")
+                .columnOrder(1)
+                .tickets(List.of(ticket))
+                .build();
+
+        List<ColumnsResponse> response = List.of(column1);
+
+        when(columnService.getColumns(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/api/teams/1/columns").with(csrf())
+                        .contentType(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(columnService).getColumns(any(Long.class));
+
+    }
 }
